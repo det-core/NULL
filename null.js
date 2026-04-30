14d5b6dfc17ab69447755977ea19a9ecc9749e64
det-core <darkempiretech@mail.com>
2026-04-30 03:36:00 +0100

    Some plugins fixed and some new features added

--- null.js
+++ null.js
@@ -19,6 +19,16 @@ const crypto = require("crypto");
 const util = require('util');
 const chalk = require('chalk');
 const { addPremiumUser, delPremiumUser } = require("./lib/premium");
+const sleep = async (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+function runtime(seconds) {
+  seconds = Number(seconds);
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+  return `${d > 0 ? d + "d " : ""}${h > 0 ? h + "h " : ""}${m > 0 ? m + "m " : ""}${s}s`;
+}
 //===============
 module.exports = async (waSocket, message, chatUpdate, store) => {
 
